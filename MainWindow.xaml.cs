3faf6a16f5303cf52c1f5ac8d7fafa3d24b3a83e
Pablo Figueroa <pfiguero@me.com>
2018-08-30 15:28:41 -0500

    Opening a json file. Adding a nuget library for that

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -16,6 +16,7 @@ namespace Microsoft.Samples.Kinect.DepthBasics
     using System.Windows.Media;
     using System.Windows.Media.Imaging;
     using Microsoft.Kinect;
+    using Newtonsoft.Json;
 
     /// <summary>
     /// Interaction logic for MainWindow
@@ -107,6 +108,22 @@ namespace Microsoft.Samples.Kinect.DepthBasics
 
             // initialize the components (controls) of the window
             this.InitializeComponent();
+
+            // Open the json file
+            var json = new StreamReader(Path.Combine(Environment.CurrentDirectory, @"..\..\..\data\50anios.json"));
+            JsonTextReader reader = new JsonTextReader(json);
+            while (reader.Read())
+            {
+                if (reader.Value != null)
+                {
+                    Debug.WriteLine("Token: {0}, Value: {1}", reader.TokenType, reader.Value);
+                }
+                else
+                {
+                    Debug.WriteLine("Token: {0}", reader.TokenType);
+                }
+            }
+
         }
 
         /// <summary>
