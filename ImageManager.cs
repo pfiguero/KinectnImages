1e3a29b3e4ee428820d2f158df68f4e35b0e6358
Pablo Figueroa <pfiguero@me.com>
2018-09-12 16:24:51 -0500

    Starting this refactoring, by splitting classes into distinct files.

--- ImageManager.cs
+++ ImageManager.cs
@@ -1,27 +1,84 @@
 ﻿namespace Pfiguero.Samples.ImageReel
 {
     using System;
+    using System.Diagnostics;
+    using System.IO;
+    using System.Windows.Media;
+    using System.Windows.Media.Imaging;
     using Newtonsoft.Json;
 
     public class ImageManager
     {
+        struct ImageInfo
+        {
+            public String filename;
+            public String title;
+        }
+        struct MyFile
+        {
+            public String directory;
+            public ImageInfo[] images;
+        }
+
+        public struct InfoReel
+        {
+            public ImageSource image;
+            public int xPos;
+            public int yPos;
+        }
+
+        public InfoReel[] reel = null;
+
+        public int LastPos { get; }
+
+        private void WriteTestData()
+        {
+            // MyFile f = { "dir1", { { "f1", "t1" }, { "f2", "t2" } } };
+            MyFile f = new MyFile();
+            f.directory = "dir1";
+            f.images = new ImageInfo[2];
+            f.images[0] = new ImageInfo();
+            f.images[0].filename = "f1";
+            f.images[0].title = "i1";
+            f.images[1] = new ImageInfo();
+            f.images[1].filename = "f1";
+            f.images[1].title = "i1";
+
+            String s = JsonConvert.SerializeObject(f);
+            Debug.WriteLine("@@@ " + s);
+        }
+
         public ImageManager()
         {
-            // Open the json file
-            var json = new StreamReader(Path.Combine(Environment.CurrentDirectory, @"..\..\..\data\50anios.json"));
-            JsonTextReader reader = new JsonTextReader(json);
-            while (reader.Read())
+            //// Background
+            //Uri uri = new System.Uri(Path.Combine(Environment.CurrentDirectory, @"..\..\..\data\PromociónRedes.jpg"));
+            //ImageSource imageSource = new BitmapImage(uri);
+            // Open the json file and de serialize it
+            MyFile f = JsonConvert.DeserializeObject<MyFile>(File.ReadAllText(System.IO.Path.Combine(Environment.CurrentDirectory, @"..\..\..\data\50anios.json")));
+
+            reel = new InfoReel[f.images.Length];
+            Uri uri;
+            for (int i = 0; i < reel.Length; i++)
             {
-                if (reader.Value != null)
-                {
-                    Debug.WriteLine("Token: {0}, Value: {1}", reader.TokenType, reader.Value);
-                }
-                else
-                {
-                    Debug.WriteLine("Token: {0}", reader.TokenType);
-                }
+                String s = @"..\..\..\images\" + f.directory + "\\" + f.images[i].filename;
+                uri = new System.Uri(System.IO.Path.Combine(Environment.CurrentDirectory, s));
+                reel[i].image = new BitmapImage(uri);
+                Debug.WriteLine("Width: " + reel[i].image.Width + " Height: " + reel[i].image.Height);
             }
 
+            // Define positions in the reel...
+            int marginX = 30;
+            int screenHeight = 720;
+            reel[0].xPos = marginX;
+            reel[0].yPos = (int)(screenHeight - reel[0].image.Height) / 2;
+            for (int i = 1; i < reel.Length; i++)
+            {
+                reel[i].xPos = (int)(reel[i - 1].xPos + reel[i - 1].image.Width + marginX);
+                reel[i].yPos = (int)(screenHeight - reel[i].image.Height) / 2;
+            }
+            LastPos = (int)(reel[reel.Length - 1].xPos + reel[reel.Length - 1].image.Width + marginX);
         }
+        //'C:\Users\COLIVRI\Documents\KinectnImages\images\50anios\002-22.jpg'.'
     }
+
 }
--- KinectManager.cs
+++ KinectManager.cs
@@ -1,10 +1,21 @@
 ﻿namespace Pfiguero.Samples.ImageReel
 {
     using System;
+    using System.Diagnostics;
+    using System.Windows.Media;
+    using System.Windows.Media.Imaging;
     using Microsoft.Kinect;
 
-    public class KinectManager: INotifyPropertyChanged
+    /// <summary>
+    /// This event is triggered when new info from the kinect is available.
+    /// </summary>
+    public delegate void MyRefreshScreenHandler(object source, EventArgs e);
+
+    public class KinectManager : IDisposable
     {
+
+        public event MyRefreshScreenHandler OnRefresh;
+
         /// <summary>
         /// Map depth range to byte range
         /// </summary>
@@ -27,6 +38,13 @@
 
         private static int frameCount = 0;
 
+        /// <summary>
+        /// this is the image we modify and copy at the end to outBitmap
+        /// </summary>
+        private BitmapFrame image = null;
+
+        private byte[] pixels = null;
+
         public int Width
         {
             get
@@ -43,21 +61,28 @@
             }
         }
 
-        private bool _depthFrameProcessed = false;
-
-        public bool hasDepthInfo()
+        public byte[] Pixels
         {
-            return this._depthFrameProcessed;
+            get
+            {
+                return pixels;
+            }
         }
 
-        public void depthInfoProcessed()
+        public uint GetBytesPerPixel()
         {
-            this._depthFrameProcessed = false;
+            return this.depthFrameDescription.BytesPerPixel;
         }
 
+        private int _howMuch = 100;
+
+        public int HowMuch { get { return _howMuch; } }
 
         public KinectManager()
         {
+            // this is the image we modify and copy at the end to outBitmap
+            image = null;
+
             // get the kinectSensor object
             this.kinectSensor = KinectSensor.GetDefault();
 
@@ -71,25 +96,172 @@
             this.depthFrameDescription = this.kinectSensor.DepthFrameSource.FrameDescription;
 
             // set IsAvailableChanged event notifier
-            this.kinectSensor.IsAvailableChanged += this.Sensor_IsAvailableChanged;
+            // this.kinectSensor.IsAvailableChanged += this.Sensor_IsAvailableChanged;
 
             // open the sensor
             this.kinectSensor.Open();
 
-
+            _howMuch = 100;
         }
 
         /// <summary>
-        /// Handles the event which the sensor becomes unavailable (E.g. paused, closed, unplugged).
+        /// Directly accesses the underlying image buffer of the DepthFrame to 
+        /// create a displayable bitmap.
+        /// This function requires the /unsafe compiler option as we make use of direct
+        /// access to the native memory pointed to by the depthFrameData pointer.
         /// </summary>
-        /// <param name="sender">object sending the event</param>
-        /// <param name="e">event arguments</param>
-        private void Sensor_IsAvailableChanged(object sender, IsAvailableChangedEventArgs e)
+        /// <param name="depthFrameData">Pointer to the DepthFrame image data</param>
+        /// <param name="depthFrameDataSize">Size of the DepthFrame image data</param>
+        /// <param name="minDepth">The minimum reliable depth value for the frame</param>
+        /// <param name="maxDepth">The maximum reliable depth value for the frame</param>
+        private unsafe void ProcessDepthFrameDataOLD(IntPtr depthFrameData, uint depthFrameDataSize, ushort minDepth, ushort maxDepth)
         {
-            // on failure, set the status text
-            //this.StatusText = this.kinectSensor.IsAvailable ? Properties.Resources.RunningStatusText
-            //                                                : Properties.Resources.SensorNotAvailableStatusText;
+            // depth frame data is a 16 bit value
+            ushort* frameData = (ushort*)depthFrameData;
+
+            image.CopyPixels(pixels, image.PixelWidth * 4, 0);
+
+            // modify the pixels, according to what is read from the Kinect
+            for (int i = 0; i < pixels.Length / 4; ++i)
+            {
+                // coordinates of the current pixel in the output image
+                int col = i % image.PixelWidth;
+                int row = i / image.PixelHeight;
+
+                // convert to the space of the Kinect image
+                int rE = (int)(((double)col * this.Width) / ((double)image.PixelWidth));
+                int cE = (int)(((double)row * this.Height) / ((double)image.PixelHeight));
+                int iE = rE + cE * this.Width; // * this.depthFrameDescription.BytesPerPixel;
+
+                ushort depth = 0;
+                if (iE < (int)(depthFrameDataSize / this.GetBytesPerPixel()))
+                {
+                    depth = frameData[iE];
+                }
+                byte depthByte = (byte)(depth >= minDepth && depth <= maxDepth ? (depth / MapDepthToByte) : 0);
+                //Debug.WriteLine("iE: " + iE + "depth: " + depth + "depthByte" + depthByte );
+
+                byte b = pixels[i * 4];
+                byte g = pixels[i * 4 + 1];
+                byte r = pixels[i * 4 + 2];
+                byte a = pixels[i * 4 + 3];
+
+                Color c = Color.FromArgb(a, r, g, b);
+                double hue, sat, val;
+                ColorToHSV(c, out hue, out sat, out val);
+                hue = ((double)depthByte / 255d) + hue;
+
+                c = ColorFromHSV(hue, sat, val);
+
+                // colors
+                //pixels[i * 4] = c.B;
+                //pixels[i * 4 + 1] = c.G;
+                //pixels[i * 4 + 2] = c.R;
+                //pixels[i * 4 + 3] = c.A;
+
+                // grays
+                //pixels[i * 4] = depthByte;
+                //pixels[i * 4 + 1] = depthByte;
+                //pixels[i * 4 + 2] = depthByte;
+                //pixels[i * 4 + 3] = 255;
+            }
         }
+
+        public double val = 0;
+        public double stdDev = 20000; // from a small sample...
+        public int nVal = 0;
+
+
+        private unsafe void ProcessDepthFrameData(IntPtr depthFrameData, uint depthFrameDataSize, ushort minDepth, ushort maxDepth)
+        {
+            // depth frame data is a 16 bit value
+            ushort* frameData = (ushort*)depthFrameData;
+
+            image.CopyPixels(pixels, image.PixelWidth * 4, 0);
+
+            // The sum of a row is an indication of what is in front...
+            int r = this.Width / 2;
+            uint sum = 0;
+            for (int c = 0; c < this.Height; c++)
+            {
+                int i = r + c * this.Width; // * this.depthFrameDescription.BytesPerPixel;
+                ushort depth = 0;
+                if (i < (int)(depthFrameDataSize / this.GetBytesPerPixel()))
+                {
+                    depth = frameData[i];
+                }
+                sum += depth;
+            }
+            // Debug.WriteLine("SUM: " + sum);
+            // sample of the 1st 10 values... Assuming nobody is in front...
+            if (nVal < 10)
+            {
+                nVal++;
+                val += sum;
+            }
+            else if (nVal == 10)
+            {
+                //stdDev = 0.1; // Math.Sqrt((nVal)/nVal);
+                val /= nVal;
+                Debug.WriteLine("VAL PROM: " + val);
+                nVal++; // just to put it outside this loop
+            }
+            else
+            {
+                if (Math.Abs(val - sum) > stdDev)
+                {
+                    _howMuch = 10;
+                }
+            }
+        }
+
+
+        public static void ColorToHSV(Color color, out double hue, out double saturation, out double value)
+        {
+            int max = Math.Max(color.R, Math.Max(color.G, color.B));
+            int min = Math.Min(color.R, Math.Min(color.G, color.B));
+
+            //hue = color.GetHue();
+            // From https://stackoverflow.com/questions/23090019/fastest-formula-to-get-hue-from-rgb
+            if (max == color.R)
+                hue = (color.G - color.B) / ((max - min) * 255d);
+            else if (max == color.G)
+                hue = 2.0 + (color.B - color.R) / ((max - min) * 255d);
+            else
+                hue = 4.0 + (color.R - color.G) / ((max - min) * 255d);
+
+            hue *= 60;
+            hue = hue < 0 ? hue + 360 : hue;
+
+            saturation = (max == 0) ? 0 : 1d - (1d * min / max);
+            value = max / 255d;
+        }
+
+        public static Color ColorFromHSV(double hue, double saturation, double value)
+        {
+            int hi = (int)(Math.Floor(hue / 60)) % 6;
+            double f = hue / 60 - Math.Floor(hue / 60);
+
+            value = value * 255;
+            byte v = (byte)(value);
+            byte p = (byte)(value * (1 - saturation));
+            byte q = (byte)(value * (1 - f * saturation));
+            byte t = (byte)(value * (1 - (1 - f) * saturation));
+
+            if (hi == 0)
+                return Color.FromArgb(255, v, t, p);
+            else if (hi == 1)
+                return Color.FromArgb(255, q, v, p);
+            else if (hi == 2)
+                return Color.FromArgb(255, p, v, t);
+            else if (hi == 3)
+                return Color.FromArgb(255, p, q, v);
+            else if (hi == 4)
+                return Color.FromArgb(255, t, p, v);
+            else
+                return Color.FromArgb(255, v, p, q);
+        }
+
 
         /// <summary>
         /// Handles the depth frame data arriving from the sensor
@@ -98,6 +270,8 @@
         /// <param name="e">event arguments</param>
         private void Reader_FrameArrived(object sender, DepthFrameArrivedEventArgs e)
         {
+            bool depthFrameProcessed = false;
+
             if (frameCount++ % 10 != 0)
                 return;
 
@@ -122,7 +296,7 @@
                             //// maxDepth = depthFrame.DepthMaxReliableDistance
 
                             this.ProcessDepthFrameData(depthBuffer.UnderlyingBuffer, depthBuffer.Size, depthFrame.DepthMinReliableDistance, maxDepth);
-                            _depthFrameProcessed = true;
+                            depthFrameProcessed = true;
                         }
                     }
                 }
@@ -130,10 +304,51 @@
 
             if (depthFrameProcessed)
             {
-                this.RenderDepthPixels();
+                OnRefresh(this, new EventArgs());
+                _howMuch = _howMuch >= 100 ? _howMuch : _howMuch + 1;
+                // Debug.WriteLine("How Much: " + _howMuch);
+
             }
         }
 
+        private bool disposed = false;
 
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposed)
+            {
+                if (this.depthFrameReader != null)
+                {
+                    // DepthFrameReader is IDisposable
+                    this.depthFrameReader.Dispose();
+                    this.depthFrameReader = null;
+                }
+
+                if (this.kinectSensor != null)
+                {
+                    this.kinectSensor.Close();
+                    this.kinectSensor = null;
+                }
+            }
+            //dispose unmanaged resources
+            disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        public void SetImage(BitmapFrame bitmapFrame)
+        {
+            image = bitmapFrame;
+        }
+
+        public void SetPixels(int v)
+        {
+            pixels = new byte[v];
+        }
     }
+
 }
